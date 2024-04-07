2a51f24913b6eeb40dc147183743638786d877d3
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-04-07 20:43:09 +0530

    Added Localstorage

--- App.jsx
+++ App.jsx
@@ -1,11 +1,59 @@
 import {View, Alert} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Balance from './src/components/Balance';
 import Category from './src/components/Category';
 import ExpenseScreen from './src/components/ExpenseScreen';
 import IncomeScreen from './src/components/IncomeScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const App = () => {
+  useEffect(() => {
+    const loadDataFromStorage = async () => {
+      const storedExpenses = await loadData('expenses');
+      if (storedExpenses) {
+        setExpenses(storedExpenses);
+      }
+      const storedIncomes = await loadData('incomes');
+      if (storedIncomes) {
+        setIncomes(storedIncomes);
+      }
+      const storedTotalIncome = await loadData('totalIncome');
+      if (storedTotalIncome) {
+        settotalIncome(storedTotalIncome);
+      }
+      const storedTotalExpense = await loadData('totalExpense');
+      if (storedTotalExpense) {
+        settotalExpense(storedTotalExpense);
+      }
+      const storedBalance = await loadData('balance');
+      if (storedBalance) {
+        setBalance(storedBalance);
+      }
+    };
+    loadDataFromStorage();
+  }, []);
+
+  const saveData = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, JSON.stringify(value));
+    } catch (e) {
+      console.error('Error saving data:', e);
+    }
+  };
+
+  const loadData = async key => {
+    try {
+      const value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        return JSON.parse(value);
+      }
+      return null;
+    } catch (e) {
+      console.error('Error loading data:', e);
+      return null;
+    }
+  };
+
   const [balance, setBalance] = useState(0);
   const [totalIncome, settotalIncome] = useState(0);
   const [totalExpense, settotalExpense] = useState(0);
@@ -24,18 +72,25 @@ const App = () => {
     if (!newIncomeDescription || !newIncomeAmount) {
       return;
     }
-    setIncomes(prevIncomes => [
-      ...prevIncomes,
+    const updatedIncomes = [
+      ...incomes,
       {
         sign: '+',
         description: newIncomeDescription,
         amount: parseFloat(newIncomeAmount),
       },
-    ]);
-    settotalIncome(
-      prevtotalIncome => prevtotalIncome + parseFloat(newIncomeAmount),
-    );
-    setBalance(prevBalance => prevBalance + parseFloat(newIncomeAmount));
+    ];
+    const updatedTotalIncome = totalIncome + parseFloat(newIncomeAmount);
+    const updatedBalance = balance + parseFloat(newIncomeAmount);
+
+    setIncomes(updatedIncomes);
+    settotalIncome(updatedTotalIncome);
+    setBalance(updatedBalance);
+
+    saveData('incomes', updatedIncomes);
+    saveData('totalIncome', updatedTotalIncome);
+    saveData('balance', updatedBalance);
+
     setModalVisible(false);
     setNewIncomeDescription('');
     setNewIncomeAmount('');
@@ -52,20 +107,26 @@ const App = () => {
     if (!newExpenseDescription || !newExpenseAmount) {
       return;
     }
-    setExpenses(prevExpenses => [
-      ...prevExpenses,
+    const updatedExpenses = [
+      ...expenses,
       {
         sign: '-',
         description: newExpenseDescription,
         amount: parseFloat(newExpenseAmount),
       },
-    ]);
-    settotalExpense(
-      prevtotalExpense => prevtotalExpense + parseFloat(newExpenseAmount),
-    );
-    setBalance(prevBalance => prevBalance - parseFloat(newExpenseAmount));
-    setModalVisible(false);
+    ];
+    const updatedTotalExpense = totalExpense + parseFloat(newExpenseAmount);
+    const updatedBalance = balance - parseFloat(newExpenseAmount);
 
+    setExpenses(updatedExpenses);
+    settotalExpense(updatedTotalExpense);
+    setBalance(updatedBalance);
+
+    saveData('expenses', updatedExpenses);
+    saveData('totalExpense', updatedTotalExpense);
+    saveData('balance', updatedBalance);
+
+    setModalVisible(false);
     setNewExpenseDescription('');
     setNewExpenseAmount('');
   };
